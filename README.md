d9d9ab773eac3e941d52dec0428f6184107379db
Yvelchrome <yvelchrome@gmail.com>
2023-06-27 11:42:58 +0200

    feat: frontend starter kit
    React/TypeScript/Tailwind(+DaisyUI)/SCSS

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
+
 # PDF_Web2
 
-slt lékip
+## Frontend
+
+### Installation
+
+1. Clone the repository
+2.  Open your terminal and execute `cd client`
+3. run `npm install`
+
+### Running the project
+
+1. Make sure you are in the `client` folder
+2. run `npm run dev` to start the project in development mode (it should run on the port 8080 by default)
+
+### Building the project
+
+1. Make sure you are in the `client` folder
+2. run `npm run build` to build the project (it should create a dist folder inside the client folder)
+    -  Additionally, run `npm run start` to start the project from the `dist` folder in production mode (it should run on the port 3000 by default)
+
+### Testing the project 
+**_( Not implemented yet)_**
+
+1. Make sure you are in the `client` folder
+2. run `npm run test` to run the tests
+
---
+++ client/webpack.config.js
@@ -0,0 +1,91 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+// const BundleAnalyzerPlugin =
+//   require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
+module.exports = (env, options) => {
+  const isProduction = options.mode === "production";
+
+  return {
+    entry: path.join(__dirname, "src", "index.tsx"),
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      publicPath: "/",
+    },
+    stats: {
+      errors: true,
+      warnings: false,
+      assets: false,
+      modules: false,
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, "public", "index.html"),
+        favicon: path.join(__dirname, "public", "favicon.ico"),
+      }),
+      isProduction && new MiniCssExtractPlugin(),
+      // new BundleAnalyzerPlugin(),
+    ],
+
+    module: {
+      rules: [
+        {
+          test: /\.?(js|jsx|ts|tsx)$/,
+          exclude: /node_modules/,
+          use: ["thread-loader", "babel-loader"],
+        },
+        {
+          test: /\.?(css|scss)$/,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : "style-loader",
+            {
+              loader: "css-loader",
+              options: { importLoaders: 2, sourceMap: true },
+            },
+            "postcss-loader",
+            { loader: "sass-loader", options: { sourceMap: true } },
+          ],
+        },
+      ],
+    },
+
+    devServer: {
+      historyApiFallback: true,
+      port: "auto",
+      open: false,
+      client: {
+        overlay: {
+          errors: true,
+          warnings: false,
+          runtimeErrors: true,
+        },
+      },
+    },
+
+    optimization: {
+      minimize: true,
+      minimizer: [
+        new CssMinimizerPlugin({
+          parallel: true,
+        }),
+        isProduction &&
+          new TerserPlugin({
+            parallel: true,
+            terserOptions: {
+              compress: {
+                drop_console: true,
+              },
+            },
+          }),
+      ],
+    },
+
+    resolve: {
+      extensions: [".js", ".jsx", ".ts", ".tsx"],
+    },
+  };
+};
