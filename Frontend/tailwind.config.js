e9f778e0a4dabbc75a06da30d6b2638fb9b0c77d
Yvelchrome <yvelchrome@gmail.com>
2023-07-17 14:18:07 +0200

    refactor: Satoshi font as custom tailwind font

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,4 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   plugins: [require("daisyui")],
+  theme: {
+    extend: {
+      fontFamily: {
+        satoshi: ["satoshi"],
+      },
+    },
+  },
 };
