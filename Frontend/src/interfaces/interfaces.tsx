b7380826a9011e6686323c809fec5f078cc4f18d
Ouallet-Sandie <ouallet.sandie@outlook.fr>
2023-07-17 10:54:13 +0200

    composant button

---
+++ Frontend/src/interfaces/interfaces.tsx
@@ -0,0 +1,15 @@
+interface State {
+    isTrue: boolean;
+    toggle: () => void
+}
+
+interface ButtonInterface {
+    text: string;
+    style: string;
+    icon: string | undefined;
+  }
+
+export type {
+    State,
+    ButtonInterface
+}
--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -1,9 +1,26 @@
 import React, { Fragment } from "react";
+import Button from "../components/Buttons/Button";
+import { ButtonInterface } from '../interfaces/interfaces'
+
 
 export default function Home() {
+
+  const buttonPlein : ButtonInterface = {
+    text: 'coucouuuuuu',
+    style: 'fill',
+    icon: undefined 
+}
+const buttonVide : ButtonInterface = {
+  text: 'ss',
+  style: 'outline',
+  icon: undefined 
+}
+
   return (
     <Fragment>
       <h1>Home</h1>
+      <Button props={buttonPlein}/>
+      <Button props={buttonVide}/>
     </Fragment>
   );
 }
