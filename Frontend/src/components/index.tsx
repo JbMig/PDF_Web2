4531acbee26d45d3d6899c6a464c021fe20f23d7
Ouallet-Sandie <ouallet.sandie@outlook.fr>
2023-07-19 10:05:41 +0200

    erreur avec popup en cours de correction

--- Frontend/src/components/index.tsx
+++ Frontend/src/components/index.tsx
@@ -2,7 +2,7 @@ import Button from "./Buttons/Button";
 import Profile from "./Labels/Profile";
 import Notifications from "./Labels/Notifications";
 import Label from "./Labels/Label";
-import SideBarPopup from "./Popups/SideBarPopUp";
+import SideBarPopup from "./popups/SideBarPopUp";
 import Header from "./Header";
 import SubHeader from "./SubHeader";
 import Input from "./Input";
