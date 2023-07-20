2b6d634a5784738ebc8a6f1046c3a79eba1c86f2
Yvelchrome <yvelchrome@gmail.com>
2023-07-20 17:14:51 +0200

    feat: Button add "delete" style

--- Frontend/src/components/Buttons/Button.tsx
+++ Frontend/src/components/Buttons/Button.tsx
@@ -1,5 +1,5 @@
-import React, { Fragment } from "react";
-import { ButtonInterface } from "interfaces";
+import React, { Fragment } from 'react';
+import { ButtonInterface } from 'interfaces';
 
 export default function Button({ props }: { props: ButtonInterface }) {
   function buttonContent() {
@@ -8,19 +8,19 @@ export default function Button({ props }: { props: ButtonInterface }) {
 
   let balise;
 
-  if (props.style == "fill") {
+  if (props.style == 'fill') {
     balise = (
       <button
         className="
 				text-md h-btn rounded-xl
 				border-2 border-solid
 				border-border_yellow bg-main_yellow
-				px-lg py-sm font-bold hover:border-border_yellow hover:bg-main_yellow"
+				px-lg py-sm font-bold uppercase hover:border-border_yellow hover:bg-main_yellow"
       >
         {buttonContent()}
       </button>
     );
-  } else if (props.style == "outline") {
+  } else if (props.style == 'outline') {
     balise = (
       <button
         className="
@@ -29,6 +29,19 @@ export default function Button({ props }: { props: ButtonInterface }) {
 				border-main_yellow bg-transparent px-lg py-sm font-bold
 				uppercase text-main_yellow hover:border-main_yellow hover:bg-transparent
 				"
+      >
+        {buttonContent()}
+      </button>
+    );
+  } else if (props.style == 'delete') {
+    balise = (
+      <button
+        className="
+                text-md h-btn rounded-xl
+                border-2 border-solid
+                border-red bg-red px-lg
+                py-sm font-bold uppercase text-white transition
+                duration-300 ease-in-out hover:border-darken_red hover:bg-darken_red"
       >
         {buttonContent()}
       </button>
